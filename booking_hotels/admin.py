f631fc4bec8300db422f3ed70843117c2cd0445e
Пользователь <slayer6662mail.ru@gmail.com>
2025-04-06 16:23:21 +0600

    Models.

---
+++ booking_hotels/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import *
+
+
+admin.site.register(UserProfile)
+admin.site.register(Hotel)
+admin.site.register(UserProfil)
+admin.site.register(Room)
+admin.site.register(RoomImage)
+admin.site.register(Review)
+admin.site.register(Booking)
+
+
---
+++ booking_hotels/translation.py
@@ -0,0 +1,2 @@
+from .models import Hotel
+from modeltranslation.translator import TranslationOptions,register
